9cddcc855e9ac38a7a3e399ec75fbf0b5c9aca2e
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2022-11-03 10:11:07 +0200

    :trollface:

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -15,10 +15,8 @@ import flixel.addons.transition.TransitionData;
 import haxe.Json;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
-#if MODS_ALLOWED
 import sys.FileSystem;
 import sys.io.File;
-#end
 import options.GraphicsSettingsSubState;
 //import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
